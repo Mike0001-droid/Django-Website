76cec59b1aa107df68103ab75499d8a37b251ae9
Mike0001-droid <110562192+Mike0001-droid@users.noreply.github.com>
2022-08-23 16:55:35 +0500

    Мой проект

--- mz/Application/models.py
+++ mz/Application/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 class Article(models.Model):
     title=models.CharField(max_length=255)
     summary=models.CharField(max_length=255)
@@ -7,3 +8,8 @@ class Article(models.Model):
     pubdate=models.DateTimeField()
     slug=models.CharField(max_length=255, unique=True)
 
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse ('article_page', kwargs={'slug' : self.slug})
