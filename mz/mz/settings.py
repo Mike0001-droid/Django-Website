10a0e17dc07243838882718d70fe106c54be790d
Mike0001-droid <110562192+Mike0001-droid@users.noreply.github.com>
2022-08-29 15:47:37 +0500

    Мой проект

--- mz/mz/settings.py
+++ mz/mz/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "mz.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'foo_db',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'foo_user',
+        'PASSWORD': 'foo_password',
     }
 }
 
